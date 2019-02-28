a6877dd5cc8d89dab4418488fcd304914f7262e8
JasonPeng <guo_tai@163.com>
2019-02-28 14:07:05 +0800

    测试其他配置文件手动刷新

--- ch3-1-eureka-client/src/main/java/com/springcloud/jp/controller/QueryController.java
+++ ch3-1-eureka-client/src/main/java/com/springcloud/jp/controller/QueryController.java
@@ -1,11 +1,16 @@
 package com.springcloud.jp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
+@RefreshScope
 @RestController
 @RequestMapping("/query")
 public class QueryController {
@@ -15,4 +20,15 @@ public class QueryController {
     public Object getEurekaServerUrl(){
         return eurekaClientConfigBean.getServiceUrl();
     }
+
+    @Value("${spring.application.name}")
+    public String name;
+
+    @GetMapping("/tsts")
+    public String tsts(){
+        return "nihao "+name;
+    }
+
+
+
 }
